cdaf00865b951197c64afc62afd58df417207f7d
Sanjana9837 <sanjanakumari9837@gmail.com>
2026-03-06 13:27:45 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO students VALUES
+(1,'Rahul','CSE',20),
+(2,'Priya','ECE',21),
+(3,'Aman','CSE',19);
+
+INSERT INTO courses VALUES
+(101,'Data Structures','Dr. Sharma'),
+(102,'Database Systems','Dr. Singh');
+
+INSERT INTO enrollments VALUES
+(1,1,101),
+(2,2,102),
+(3,3,101);
